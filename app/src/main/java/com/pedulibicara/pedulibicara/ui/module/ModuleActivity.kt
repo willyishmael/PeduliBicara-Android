019b80c510379bdbd5f61ce68c5e412ae113db13
willyishmael <wtangka22@gmail.com>
2022-05-26 16:01:27 +0800

    create module activity

--- app/src/main/java/com/pedulibicara/pedulibicara/ui/module/ModuleActivity.kt
+++ app/src/main/java/com/pedulibicara/pedulibicara/ui/module/ModuleActivity.kt
@@ -1,11 +1,12 @@
-package com.pedulibicara.pedulibicara
+package com.pedulibicara.pedulibicara.ui.module
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.pedulibicara.pedulibicara.R
 
-class MainActivity : AppCompatActivity() {
+class ModuleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_module)
     }
 }
---
+++ app/src/main/java/com/pedulibicara/pedulibicara/ui/module/ModuleCategoryViewModel.kt
@@ -0,0 +1,7 @@
+package com.pedulibicara.pedulibicara.ui.module
+
+import androidx.lifecycle.ViewModel
+
+class ModuleCategoryViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
