c84b208dfa6e6986861d8ec304468106901b58e4
William Tangka <wtangka22@gmail.com>
2022-05-27 10:51:55 +0800

    create guess cards activity

---
+++ app/src/main/java/com/pedulibicara/pedulibicara/ui/guesscards/GuessCardsActivity.kt
@@ -0,0 +1,12 @@
+package com.pedulibicara.pedulibicara.ui.guesscards
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pedulibicara.pedulibicara.R
+
+class GuessCardsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_guess_cards)
+    }
+}
