d12a306e8e91cb178f4cfdc73248ad524be1691b
William Tangka <wtangka22@gmail.com>
2022-05-27 15:09:46 +0800

    create guess voice activity

---
+++ app/src/main/java/com/pedulibicara/pedulibicara/ui/guessvoice/GuessVoiceActivity.kt
@@ -0,0 +1,11 @@
+package com.pedulibicara.pedulibicara.ui.guessvoice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GuessVoiceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_guess_voice)
+    }
+}
