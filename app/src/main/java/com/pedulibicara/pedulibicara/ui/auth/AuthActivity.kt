3f9f473ec78862de120e8cdb15cdd694e154ed84
willyishmael <wtangka22@gmail.com>
2022-05-29 23:14:03 +0800

    Update AuthActivity.kt

--- app/src/main/java/com/pedulibicara/pedulibicara/ui/auth/AuthActivity.kt
+++ app/src/main/java/com/pedulibicara/pedulibicara/ui/auth/AuthActivity.kt
@@ -2,10 +2,15 @@ package com.pedulibicara.pedulibicara.ui.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.pedulibicara.pedulibicara.databinding.ActivityAuthBinding
 
 class AuthActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAuthBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_auth)
+        binding = ActivityAuthBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
