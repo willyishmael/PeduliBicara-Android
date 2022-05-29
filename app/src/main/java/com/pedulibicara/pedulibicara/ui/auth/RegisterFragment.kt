ae60eed3205f826001c72b26dc85cb23da7ad010
willyishmael <wtangka22@gmail.com>
2022-05-29 22:56:20 +0800

    Update RegisterFragment.kt

--- app/src/main/java/com/pedulibicara/pedulibicara/ui/auth/RegisterFragment.kt
+++ app/src/main/java/com/pedulibicara/pedulibicara/ui/auth/RegisterFragment.kt
@@ -6,8 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import com.pedulibicara.pedulibicara.R
+import com.pedulibicara.pedulibicara.databinding.FragmentLoginBinding
+import com.pedulibicara.pedulibicara.databinding.FragmentRegisterBinding
 
+@Suppress("RedundantNullableReturnType")
 class RegisterFragment : Fragment() {
 
     companion object {
@@ -16,17 +20,39 @@ class RegisterFragment : Fragment() {
 
     private lateinit var viewModel: RegisterViewModel
 
+    private var _binding: FragmentRegisterBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        _binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    @Suppress("DEPRECATION")
+    @Deprecated("Deprecated in Java")
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(RegisterViewModel::class.java)
         // TODO: Use the ViewModel
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupButtons()
+    }
+
+    private fun setupButtons() {
+        binding.apply {
+
+            tvLogin.setOnClickListener { view ->
+                view.findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
+            }
+
+        }
     }
 
 }
