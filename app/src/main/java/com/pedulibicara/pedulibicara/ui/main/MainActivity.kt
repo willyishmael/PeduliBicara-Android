63aca651d7c25989b5dd880eb00b375235c0392f
willyishmael <wtangka22@gmail.com>
2022-05-30 00:45:15 +0800

    update activity main

--- app/src/main/java/com/pedulibicara/pedulibicara/ui/main/MainActivity.kt
+++ app/src/main/java/com/pedulibicara/pedulibicara/ui/main/MainActivity.kt
@@ -1,12 +1,46 @@
 package com.pedulibicara.pedulibicara.ui.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.pedulibicara.pedulibicara.R
+import com.pedulibicara.pedulibicara.databinding.ActivityMainBinding
+import com.pedulibicara.pedulibicara.ui.guesscards.GuessCardsActivity
+import com.pedulibicara.pedulibicara.ui.guessvoice.GuessVoiceActivity
+import com.pedulibicara.pedulibicara.ui.module.ModuleActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupActions()
+    }
+
+    private fun setupActions() {
+        binding.apply {
+
+            mcModule.mcContainer.setOnClickListener {
+                Intent(this@MainActivity, ModuleActivity::class.java).also { intent ->
+                    startActivity(intent)
+                }
+            }
+
+            mcGuessCard.mcContainer.setOnClickListener {
+                Intent(this@MainActivity, GuessCardsActivity::class.java).also { intent ->
+                    startActivity(intent)
+                }
+            }
+
+            mcGuessVoice.mcContainer.setOnClickListener {
+                Intent(this@MainActivity, GuessVoiceActivity::class.java).also { intent ->
+                    startActivity(intent)
+                }
+            }
+
+        }
     }
 }
