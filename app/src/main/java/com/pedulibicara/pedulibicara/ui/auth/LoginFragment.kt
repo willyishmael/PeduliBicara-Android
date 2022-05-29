bb47011ba3259d9649968d60089dd93706da2104
willyishmael <wtangka22@gmail.com>
2022-05-29 22:50:49 +0800

    Update LoginFragment.kt

--- app/src/main/java/com/pedulibicara/pedulibicara/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/pedulibicara/pedulibicara/ui/auth/LoginFragment.kt
@@ -6,8 +6,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import com.pedulibicara.pedulibicara.R
+import com.pedulibicara.pedulibicara.databinding.FragmentLoginBinding
 
+@Suppress("RedundantNullableReturnType")
 class LoginFragment : Fragment() {
 
     companion object {
@@ -16,17 +19,44 @@ class LoginFragment : Fragment() {
 
     private lateinit var viewModel: LoginViewModel
 
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupButtons()
+    }
+
+    @Deprecated("Deprecated in Java")
+    @Suppress("DEPRECATION")
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
+        viewModel = ViewModelProvider(this)[LoginViewModel::class.java]
         // TODO: Use the ViewModel
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    private fun setupButtons() {
+        binding.apply {
+
+            tvRegister.setOnClickListener { view ->
+                view.findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+            }
+
+        }
     }
 
 }
