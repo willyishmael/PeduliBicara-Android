5b7851ca63a5216515cece32ef65bc39f689adce
willyishmael <wtangka22@gmail.com>
2022-05-29 20:34:38 +0800

    Create Auth activity and fragments

---
+++ app/src/main/java/com/pedulibicara/pedulibicara/ui/auth/AuthActivity.kt
@@ -0,0 +1,11 @@
+package com.pedulibicara.pedulibicara.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
---
+++ app/src/main/java/com/pedulibicara/pedulibicara/ui/auth/RegisterViewModel.kt
@@ -0,0 +1,7 @@
+package com.pedulibicara.pedulibicara.ui.auth
+
+import androidx.lifecycle.ViewModel
+
+class RegisterViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
