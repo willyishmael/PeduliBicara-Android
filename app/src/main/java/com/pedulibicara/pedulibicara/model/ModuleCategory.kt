956789d8c70ac1074529060154827122edd372ef
William Tangka <wtangka22@gmail.com>
2022-05-30 11:18:58 +0800

    Create ModuleCategory.kt

---
+++ app/src/main/java/com/pedulibicara/pedulibicara/model/ModuleCategory.kt
@@ -0,0 +1,9 @@
+package com.pedulibicara.pedulibicara.model
+
+import android.media.Image
+
+data class ModuleCategory(
+    val picture: Image,
+    val title: String,
+    val description: String
+)
