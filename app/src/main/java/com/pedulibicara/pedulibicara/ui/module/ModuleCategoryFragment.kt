1c46130c615ab4ee1da4bdf21b116981d27bbae0
William Tangka <wtangka22@gmail.com>
2022-05-30 15:39:57 +0800

    Update ModuleCategoryFragment.kt

--- app/src/main/java/com/pedulibicara/pedulibicara/ui/module/ModuleCategoryFragment.kt
+++ app/src/main/java/com/pedulibicara/pedulibicara/ui/module/ModuleCategoryFragment.kt
@@ -7,7 +7,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.pedulibicara.pedulibicara.R
+import com.pedulibicara.pedulibicara.databinding.FragmentModuleCategoryBinding
 
+@Suppress("RedundantNullableReturnType")
 class ModuleCategoryFragment : Fragment() {
 
     companion object {
@@ -16,17 +18,38 @@ class ModuleCategoryFragment : Fragment() {
 
     private lateinit var viewModel: ModuleCategoryViewModel
 
+    private var _binding: FragmentModuleCategoryBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_module_category, container, false)
+        _binding = FragmentModuleCategoryBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    @Suppress("DEPRECATION")
+    @Deprecated("Deprecated in Java")
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(ModuleCategoryViewModel::class.java)
         // TODO: Use the ViewModel
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView() {
+
     }
 
 }
