a0b713b3e9a352d2f699d8f5488b9693720944ff
willyishmael <wtangka22@gmail.com>
2022-06-01 16:45:59 +0800

    Update ModuleItemDetailFragment.kt

--- app/src/main/java/com/pedulibicara/pedulibicara/ui/module/ModuleItemDetailFragment.kt
+++ app/src/main/java/com/pedulibicara/pedulibicara/ui/module/ModuleItemDetailFragment.kt
@@ -6,7 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.pedulibicara.pedulibicara.R
+import com.pedulibicara.pedulibicara.databinding.FragmentModuleItemDetailBinding
 
 class ModuleItemDetailFragment : Fragment() {
 
@@ -16,17 +16,35 @@ class ModuleItemDetailFragment : Fragment() {
 
     private lateinit var viewModel: ModuleItemDetailViewModel
 
+    private var _binding: FragmentModuleItemDetailBinding? = null
+    private val binding get() = _binding!!
+
+    @Suppress("RedundantNullableReturnType")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_module_item_detail, container, false)
+        val binding = FragmentModuleItemDetailBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    @Deprecated("Deprecated in Java")
+    @Suppress("DEPRECATION")
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(ModuleItemDetailViewModel::class.java)
         // TODO: Use the ViewModel
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
     }
 
 }
