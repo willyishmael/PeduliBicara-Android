00d7e856dc2e37a06f2720ae87e88a715b22f1a5
willyishmael <wtangka22@gmail.com>
2022-06-01 15:21:08 +0800

    update data

--- app/src/main/java/com/pedulibicara/pedulibicara/data/local/Data.kt
+++ app/src/main/java/com/pedulibicara/pedulibicara/data/local/Data.kt
@@ -2,6 +2,7 @@ package com.pedulibicara.pedulibicara.data.local
 
 import com.pedulibicara.pedulibicara.R
 import com.pedulibicara.pedulibicara.data.model.Category
+import com.pedulibicara.pedulibicara.data.model.ModuleItem
 import com.pedulibicara.pedulibicara.utils.Strings
 
 object Data {
@@ -46,7 +47,51 @@ object Data {
         )
     }
 
+    fun getModuleItem(category: String) : List<ModuleItem> {
+        val listModuleItem = mutableListOf<ModuleItem>()
+
+        for (item in getAllModuleItem()) {
+            if (item.category == category) {
+                listModuleItem.add(item)
+            }
+        }
+
+        return listModuleItem
+    }
+
+    fun getAllModuleItem() = listOf(
+        ModuleItem(
+            MODULE_BODY_PARTS,
+            Strings.get(R.string.head),
+            0,
+            0
+        ),
+        ModuleItem(
+            MODULE_BODY_PARTS,
+            Strings.get(R.string.lips),
+            0,
+            0
+        ),
+        ModuleItem(
+            MODULE_BODY_PARTS,
+            Strings.get(R.string.nose),
+            0,
+            0
+        ),
+        ModuleItem(
+            MODULE_BODY_PARTS,
+            Strings.get(R.string.eyes),
+            0,
+            0
+        ),
+
+    )
+
     const val MENU_MODULE = "menu_module"
     const val MENU_GUESS_CARD = "menu_guess_card"
     const val MENU_GUESS_VOICE = "menu_guess_voice"
+
+    const val MODULE_BODY_PARTS = "module_body_parts"
+    const val MODULE_FOOD = "module_food"
+    const val MODULE_ANIMALS = "module_animals"
 }
