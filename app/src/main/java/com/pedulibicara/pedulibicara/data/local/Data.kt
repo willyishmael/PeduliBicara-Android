72bdbd362ca28b3eb00c4ab9a91bcaef02a5302f
willyishmael <wtangka22@gmail.com>
2022-06-01 11:59:57 +0800

    Update Data.kt

--- app/src/main/java/com/pedulibicara/pedulibicara/data/local/Data.kt
+++ app/src/main/java/com/pedulibicara/pedulibicara/data/local/Data.kt
@@ -7,8 +7,8 @@ import com.pedulibicara.pedulibicara.data.model.Category
 object Data {
 
     fun getModuleCategory(context: Context?) : List<Category> {
-        if (context != null) {
-            return listOf(
+        return if (context != null) {
+            listOf(
                 Category(
                     0,
                     context.getString(R.string.body_parts),
@@ -29,4 +29,33 @@ object Data {
             return emptyList()
         }
     }
+
+    fun getMenuCategory(context: Context?) : Map<String, Category> {
+        return if (context != null) {
+            mapOf(
+                MENU_MODULE to Category(
+                    0,
+                    context.getString(R.string.module),
+                    context.getString(R.string.module_description)
+                ),
+                MENU_GUESS_CARD to Category(
+                    0,
+                    context.getString(R.string.guess_card),
+                    context.getString(R.string.guess_card_description)
+                ),
+                MENU_GUESS_VOICE to Category(
+                    0,
+                    context.getString(R.string.guess_voice),
+                    context.getString(R.string.guess_voice_description)
+                )
+            )
+        } else {
+            emptyMap()
+        }
+
+    }
+
+    const val MENU_MODULE = "menu_module"
+    const val MENU_GUESS_CARD = "menu_guess_card"
+    const val MENU_GUESS_VOICE = "menu_guess_voice"
 }
