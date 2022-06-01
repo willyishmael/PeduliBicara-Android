4a92af0f1a1de8ea94b917e04ec76a221af696e9
willyishmael <wtangka22@gmail.com>
2022-06-01 13:39:52 +0800

    Update Data.kt

--- app/src/main/java/com/pedulibicara/pedulibicara/data/local/Data.kt
+++ app/src/main/java/com/pedulibicara/pedulibicara/data/local/Data.kt
@@ -1,59 +1,49 @@
 package com.pedulibicara.pedulibicara.data.local
 
-import android.content.Context
-import android.content.res.Resources
 import com.pedulibicara.pedulibicara.R
 import com.pedulibicara.pedulibicara.data.model.Category
+import com.pedulibicara.pedulibicara.utils.Strings
 
 object Data {
 
-    private val resources = Resources.getSystem()
-
     fun getModuleCategory() : List<Category> {
-        resources.apply {
-            return listOf(
-                Category(
-                    0,
-                    getString(R.string.body_parts),
-                    getString(R.string.body_parts_description)
-                ),
-                Category(
-                    0,
-                    getString(R.string.food),
-                    getString(R.string.food_description)
-                ),
-                Category(
-                    0,
-                    getString(R.string.animals),
-                    getString(R.string.animals_description)
-                )
+        return listOf(
+            Category(
+                0,
+                Strings.get(R.string.body_parts),
+                Strings.get(R.string.body_parts_description)
+            ),
+            Category(
+                0,
+                Strings.get(R.string.food),
+                Strings.get(R.string.food_description)
+            ),
+            Category(
+                0,
+                Strings.get(R.string.animals),
+                Strings.get(R.string.animals_description)
             )
-        }
-
-
+        )
     }
 
     fun getMenuCategory() : Map<String, Category> {
-        resources.apply {
-            return mapOf(
-                MENU_MODULE to Category(
-                    0,
-                    getString(R.string.module),
-                    getString(R.string.module_description)
-                ),
-                MENU_GUESS_CARD to Category(
-                    0,
-                    getString(R.string.guess_card),
-                    getString(R.string.guess_card_description)
-                ),
-                MENU_GUESS_VOICE to Category(
-                    0,
-                    getString(R.string.guess_voice),
-                    getString(R.string.guess_voice_description)
-                )
+        return mapOf(
+            MENU_MODULE to Category(
+                0,
+                Strings.get(R.string.module),
+                Strings.get(R.string.module_description)
+            ),
+            MENU_GUESS_CARD to Category(
+                0,
+                Strings.get(R.string.guess_card),
+                Strings.get(R.string.guess_card_description)
+            ),
+            MENU_GUESS_VOICE to Category(
+                0,
+                Strings.get(R.string.guess_voice),
+                Strings.get(R.string.guess_voice_description)
             )
-
-        }
+        )
     }
 
     const val MENU_MODULE = "menu_module"
