7ac31b9d1c23f35ffee8bf73636e3efdbcaba7cb
willyishmael <wtangka22@gmail.com>
2022-06-04 22:57:51 +0800

    update set question count page

--- app/src/main/java/com/pedulibicara/pedulibicara/ui/guessvoice/SetQuestionCountFragment.kt
+++ app/src/main/java/com/pedulibicara/pedulibicara/ui/guessvoice/SetQuestionCountFragment.kt
@@ -1,32 +1,43 @@
 package com.pedulibicara.pedulibicara.ui.guessvoice
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.pedulibicara.pedulibicara.R
+import androidx.navigation.fragment.findNavController
+import com.pedulibicara.pedulibicara.databinding.FragmentSetQuestionCountBinding
 
 class SetQuestionCountFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = SetQuestionCountFragment()
-    }
-
-    private lateinit var viewModel: SetQuestionCountViewModel
+    private var _binding: FragmentSetQuestionCountBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_set_question_count, container, false)
+    ): View {
+        _binding = FragmentSetQuestionCountBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(SetQuestionCountViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupView()
+    }
+
+    private fun setupView() {
+        binding.btnNext.setOnClickListener {
+            val questionCount = binding.nbQuestionCount.value
+            val destination = SetQuestionCountFragmentDirections
+                .actionSetQuestionCountFragmentToGuessVoicePlayFragment(questionCount)
+            findNavController().navigate(destination)
+        }
+    }
 }
--- app/src/main/java/com/pedulibicara/pedulibicara/ui/guessvoice/SetQuestionCountViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.pedulibicara.pedulibicara.ui.guessvoice
-
-import androidx.lifecycle.ViewModel
-
-class SetQuestionCountViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
