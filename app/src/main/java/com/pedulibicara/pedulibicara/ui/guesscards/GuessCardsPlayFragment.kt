c8871abe9ef966f3c3cb02215593ffd61ad4a051
William Tangka <wtangka22@gmail.com>
2022-06-08 10:30:48 +0800

    Update GuessCardsPlayFragment.kt

--- app/src/main/java/com/pedulibicara/pedulibicara/ui/guesscards/GuessCardsPlayFragment.kt
+++ app/src/main/java/com/pedulibicara/pedulibicara/ui/guesscards/GuessCardsPlayFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import com.pedulibicara.pedulibicara.data.model.ModuleItem
 import com.pedulibicara.pedulibicara.databinding.FragmentGuessCardsPlayBinding
 
 class GuessCardsPlayFragment : Fragment() {
@@ -14,6 +15,8 @@ class GuessCardsPlayFragment : Fragment() {
 
     private var _binding: FragmentGuessCardsPlayBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var question: ModuleItem
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,7 +34,19 @@ class GuessCardsPlayFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        
+        viewModel.generateQuestions()
+        question = viewModel.getQuestion()
+        setupView()
+    }
+
+    private fun setupView() {
+
+    }
+
+    private fun nextQuestion() {
+        viewModel.nextQuestion()
+        question = viewModel.getQuestion()
+        setupView()
     }
 
 }
