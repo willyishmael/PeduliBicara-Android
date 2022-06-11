d5d942e8212dbe246d47bef4ba33354991c82d38
willyishmael <wtangka22@gmail.com>
2022-06-11 21:09:05 +0800

    Update GuessCardsPlayViewModel.kt

--- app/src/main/java/com/pedulibicara/pedulibicara/ui/guesscards/GuessCardsPlayViewModel.kt
+++ app/src/main/java/com/pedulibicara/pedulibicara/ui/guesscards/GuessCardsPlayViewModel.kt
@@ -2,18 +2,29 @@ package com.pedulibicara.pedulibicara.ui.guesscards
 
 import androidx.lifecycle.ViewModel
 import com.pedulibicara.pedulibicara.data.model.ModuleItem
+import com.pedulibicara.pedulibicara.data.remote.response.ModelResponse
 import com.pedulibicara.pedulibicara.data.repository.DataRepository
+import com.pedulibicara.pedulibicara.data.repository.ModelRepository
+import kotlinx.coroutines.flow.Flow
+import okhttp3.MultipartBody
 import kotlin.random.Random
 
 class GuessCardsPlayViewModel : ViewModel() {
-    private val repository = DataRepository()
-    private fun getAllModuleItems() = repository.getAllModuleItem()
-    private fun getModuleItems(category: String) = repository.getModuleItems(category)
+    private val dataRepository = DataRepository()
+    private val modelRepository = ModelRepository()
+
+    private fun getAllModuleItems() = dataRepository.getAllModuleItem()
+    private fun getModuleItems(category: String) = dataRepository.getModuleItems(category)
 
     private lateinit var questions : List<ModuleItem>
     private var currentQuestionPosition = 0
     fun getCurrentQuestionPosition() = currentQuestionPosition
 
+
+    /**
+     * Generate random list of ModuleItem
+     * @return List<ModuleItem>
+     */
     private fun getRandomModuleItems() : List<ModuleItem> {
         val list = getAllModuleItems()
         val listCount = list.count()
@@ -27,19 +38,40 @@ class GuessCardsPlayViewModel : ViewModel() {
         return randomList
     }
 
+    /**
+     * Generate questions
+     *
+     */
     fun generateQuestions() {
         questions = getRandomModuleItems()
     }
 
+    /**
+     * Get a question(ModuleItem) based on current position
+     * @return ModuleItem
+     */
     fun getQuestion() : ModuleItem {
         return questions[currentQuestionPosition]
     }
 
+    /**
+     * Next question
+     *
+     */
     fun nextQuestion() {
         if (currentQuestionPosition < QUESTION_COUNT) {
             currentQuestionPosition += 1
         }
     }
+
+    /**
+     * Upload voice file to machine learning model
+     * @param file Part: recorded voice
+     * @return Flow<Result<ModelResponse>>
+     */
+    suspend fun predictVoice(
+        file: MultipartBody.Part
+    ): Flow<Result<ModelResponse>> = modelRepository.predict(file)
 
     companion object {
         const val QUESTION_COUNT = 5
